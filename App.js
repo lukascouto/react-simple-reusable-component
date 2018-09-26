dcd54fe80b847289acc6fac6a8e5cc48405ec76f
lukascouto <lukascoutoo@gmail.com>
2018-09-26 02:37:04 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,38 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+    render() {
 
-    class PeopleList extends Component {
-        render() {
+        class PeopleList extends Component {
+            render() {
 
-            const people = this.props.names
+                const people = this.props.names
 
-            return <ul>
-                {people.map((person) => (
-                    <li key={person.name}>{person.name}</li>
-                ))}
-            </ul>
+                return <ul>
+                    {people.map((person) => (
+                        <li key={person.name}>{person.name}</li>
+                    ))}
+                </ul>
+            }
         }
+
+        return (
+            <div className="App">
+                <h1>People List</h1>
+                <h2>List One</h2>
+                <PeopleList names={[
+                    {name: 'Lukas'},
+                    {name: 'Maria'},
+                    {name: 'Pedro'}
+                ]}/>
+                <h2>List Two</h2>
+                <PeopleList names={[
+                    { name: 'Carlos' },
+                    { name: 'Junior' },
+                    { name: 'Carla' }
+                ]}/>
+            </div>
+        );
     }
-    return (
-      <div className="App">
-          <h1>People List</h1>
-          <h2>List One</h2>
-          <PeopleList names={[
-              {name: 'Lukas'},
-              {name: 'Maria'},
-              {name: 'Pedro'}
-          ]}/>
-          <h2>List One</h2>
-          <PeopleList names={[
-              { name: 'Carlos' },
-              { name: 'Junior' },
-              { name: 'Carla' }
-          ]}/>
-      </div>
-    );
-  }
 }
 
 export default App;
